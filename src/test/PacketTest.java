84483ad2b8d87f1ae7035b1afdf250bd0150a6ca
theomn <theo_menard@hotmail.fr>
2016-10-28 21:29:58 -0400

    packettest change

--- src/test/PacketTest.java
+++ src/test/PacketTest.java
@@ -2,7 +2,7 @@ package test;
 
 import org.jnetpcap.packet.JMemoryPacket;
 import org.jnetpcap.packet.JPacket;
-import org.jnetpcap.packet.PcapPacket;
+import org.jnetpcap.packet.format.FormatUtils;
 import org.jnetpcap.protocol.JProtocol;
 import org.jnetpcap.protocol.lan.Ethernet;
 import org.jnetpcap.protocol.network.Ip4;
@@ -16,7 +16,12 @@ public class PacketTest {
 						+ " ca30c3ef 008f2e80 11f52ea8 4b578018 " + " ffffa6ea 00000101 080a152e ef03002a "
 						+ " 2c943538 322e3430 204e4f4f 500d0a");
 
-		PcapPacket pp;
+		/*PcapPacket pp;
+		
+		Ip4 ip4;
+		if (pp.hasHeader(ip4)){
+			ip4.destination();
+		}*/
 		
 		Ip4 ip = packet.getHeader(new Ip4());
 		Tcp tcp = packet.getHeader(new Tcp());
@@ -26,6 +31,8 @@ public class PacketTest {
 		ip.checksum(ip.calculateChecksum());
 		tcp.checksum(tcp.calculateChecksum());
 		packet.scan(Ethernet.ID);
+		
+		System.out.println(FormatUtils.ip(ip.destination()));
 
 		System.out.println(packet);
 	}
