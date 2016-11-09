bae5e9912733ae99a5165835d613ec74a6526648
theomn <theo_menard@hotmail.fr>
2016-11-09 17:15:52 -0500

    minor commit

--- src/netp/SignatureUpdate.java
+++ src/netp/SignatureUpdate.java
@@ -5,9 +5,11 @@ import java.util.Date;
 import java.util.Queue;
 
 import org.jnetpcap.packet.JPacket;
+import org.jnetpcap.packet.Payload;
 import org.jnetpcap.packet.format.FormatUtils;
 import org.jnetpcap.protocol.network.Ip4;
 import org.jnetpcap.protocol.tcpip.Tcp;
+import org.jnetpcap.protocol.tcpip.Udp;
 
 import ca.uqac.lif.cep.Processor;
 import ca.uqac.lif.cep.SingleProcessor;
@@ -17,7 +19,10 @@ public class SignatureUpdate extends SingleProcessor {
 	protected Date firstPacketTime;
 	protected Date lastPacketTime;
 	protected FlowSignature lastSignature;
-	
+
+	protected static final int TCP = 6;
+	protected static final int UDP = 17;
+
 	public SignatureUpdate() {
 		super(1, 1);
 		firstPacketTime = null;
@@ -27,30 +32,56 @@ public class SignatureUpdate extends SingleProcessor {
 
 	@Override
 	protected Queue<Object[]> compute(Object[] inputs) {
-		JPacket packet = (JPacket)inputs[0];
+		JPacket packet = (JPacket) inputs[0];
 		FlowSignature signature = new FlowSignature();
 		Date currentPacketTime = new Date(packet.getCaptureHeader().timestampInMillis());
-		if (firstPacketTime == null){
+		if (firstPacketTime == null) {
 			firstPacketTime = currentPacketTime;
 		}
-		
+
 		Ip4 ip4 = new Ip4();
 		if (!packet.hasHeader(ip4)) {
 			System.out.println("No IP4 header in filtered packet");
 			return new ArrayDeque<Object[]>();
 		}
 		
-		Tcp tcp = new Tcp();
-		if (!packet.hasHeader(tcp)) {
-			System.out.println("no TCP header in filtered packet");
-			return new ArrayDeque<Object[]>();
+		signature.setSourceIp(FormatUtils.ip(ip4.source()));
+		signature.setDestinationIp(FormatUtils.ip(ip4.destination()));
+
+		signature.setProtocol(ip4.type());
+
+		if (signature.getProtocol() == TCP) {
+			Tcp tcp = new Tcp();
+			if (packet.hasHeader(tcp)) {
+				signature.setSourcePort(tcp.source());
+				signature.setDestinationPort(tcp.destination());
+			} else {
+				System.out.println("no TCP header in packet");
+				return new ArrayDeque<Object[]>();
+			}
 		}
 		
-		signature.setSourceIp(FormatUtils.ip(ip4.source()));
-		signature.setSourcePort(tcp.source());
-		signature.setDestinationIp(FormatUtils.ip(ip4.destination()));
-		signature.setDestinationPort(tcp.destination());
-		return null;
+		if (signature.getProtocol() == UDP) {
+			Udp udp = new Udp();
+			if (packet.hasHeader(udp)) {
+				signature.setSourcePort(udp.source());
+				signature.setDestinationPort(udp.destination());
+			} else {
+				System.out.println("no UDP header in packet");
+				return new ArrayDeque<Object[]>();
+			}
+		}
+		
+		int payloadLength;
+		Payload payload = new Payload();
+		if (packet.hasHeader(payload)) {
+			payloadLength = payload.getLength();
+		}
+		
+		if (lastSignature == null){
+			
+		}
+		
 	}
 
 	@Override
