b29a02cccca566f22831fe358dc8f5de2e454f95
theomn <theo_menard@hotmail.fr>
2016-11-30 15:05:15 -0500

    PacketFilter

---
+++ src/netp/PacketFilter.java
@@ -0,0 +1,182 @@
+package netp;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+
+import org.jnetpcap.packet.JPacket;
+import org.jnetpcap.protocol.network.Ip4;
+import org.jnetpcap.protocol.tcpip.Tcp;
+import org.jnetpcap.protocol.tcpip.Udp;
+
+import ca.uqac.lif.cep.Processor;
+import ca.uqac.lif.cep.SingleProcessor;
+
+public class PacketFilter extends SingleProcessor{
+
+	public static final int TCP = 6;
+	public static final int UDP = 17;
+	public static final int ANY_PROTOCOL = 0;
+	
+	private int protocol;
+	private ArrayList<byte[]> sourceIp;
+	private int sourcePortLow;
+	private int sourcePortHigh;
+	private ArrayList<byte[]> destinationIp;
+	private int destinationPortLow;
+	private int destinationPortHigh;
+
+	private Ip4 ip4;
+	int tmpProtocol;
+	private Tcp tcp;
+	private Udp udp;
+	private boolean found;
+	
+	public PacketFilter() {
+		super(1, 1);
+		protocol = ANY_PROTOCOL;
+		sourcePortLow = 0;
+		sourcePortHigh = 65535;
+		destinationPortLow = 0;
+		destinationPortHigh = 65535;
+	}
+	
+	public void addSourceIp(String src){
+		try {
+			InetAddress ip = InetAddress.getByName(src);
+			byte[] b= ip.getAddress();
+			sourceIp.add(b);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void removeSourceIp(String src){
+		try {
+			InetAddress ip = InetAddress.getByName(src);
+			byte[] b= ip.getAddress();
+			sourceIp.remove(b);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void addDestinationIp(String dst){
+		try {
+			InetAddress ip = InetAddress.getByName(dst);
+			byte[] b= ip.getAddress();
+			destinationIp.add(b);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void removeDestinationIp(String dst){
+		try {
+			InetAddress ip = InetAddress.getByName(dst);
+			byte[] b= ip.getAddress();
+			destinationIp.remove(b);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void setProtocol(int protocol){
+		this.protocol = protocol;
+	}
+	
+	public void setSourcePort(int src){
+		sourcePortLow = src;
+		sourcePortHigh = src;
+	}
+	
+	public void setSourcePortRange(int low, int high){
+		sourcePortLow = low;
+		sourcePortHigh = high;
+	}
+	
+	public void setDestinationPort(int dst){
+		destinationPortLow = dst;
+		destinationPortHigh = dst;
+	}
+	
+	public void setDestinationPortRange(int low, int high){
+		destinationPortLow = low;
+		destinationPortHigh = high;
+	}
+
+	@Override
+	protected Queue<Object[]> compute(Object[] inputs) {
+		JPacket packet = (JPacket)inputs[0];
+		ip4 = new Ip4(); // retrieve IP header
+		if (!packet.hasHeader(ip4)) {
+			return new ArrayDeque<Object[]>();
+		}
+
+		// check protocol
+		if (protocol > 0 && ip4.type() != protocol) {
+			return new ArrayDeque<Object[]>();
+		}
+		
+		tmpProtocol = ip4.type();
+
+		// check source and destination ports
+		if (tmpProtocol == TCP) {
+			tcp = new Tcp();
+			if (!packet.hasHeader(tcp)) {
+				return new ArrayDeque<Object[]>();
+			}
+			if (!(sourcePortLow <= tcp.source() && tcp.source() <= sourcePortHigh)
+					|| !(destinationPortLow <= tcp.destination() && tcp.destination() <= destinationPortHigh)) {
+				return new ArrayDeque<Object[]>();
+			}
+		}
+		if (tmpProtocol == UDP) {
+			udp = new Udp();
+			if (!packet.hasHeader(udp)) {
+				return new ArrayDeque<Object[]>();
+			}
+			if (!(sourcePortLow <= udp.source() && udp.source() <= sourcePortHigh)
+					|| !(destinationPortLow <= udp.destination() && udp.destination() <= destinationPortHigh)) {
+				return new ArrayDeque<Object[]>();
+			}
+		}
+
+		// check source and destination IPs
+		found = false;
+		for (byte[] ip : sourceIp){
+			if (ip.equals(ip4.source())){
+				found = true;
+				break;
+			}
+		}
+		if (!found){
+			return new ArrayDeque<Object[]>();
+		}
+		
+		found = false;
+		for (byte[] ip : destinationIp){
+			if (ip.equals(ip4.destination())){
+				found = true;
+				break;
+			}
+		}
+		if (!found){
+			return new ArrayDeque<Object[]>();
+		}
+
+		// Packet went through the filter
+		Object[] out = new Object[1];
+		out[0] = packet;
+		return wrapVector(out);
+	}
+
+	@Override
+	public Processor clone() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
