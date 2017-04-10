0e1adfb325f54dcb8a739e9fc92917b406189e71
theomn <theo_menard@hotmail.fr>
2017-04-10 13:28:55 -0400

    AllDifferent.java

--- src/netp/AllDifferent.java
+++ src/netp/AllDifferent.java
@@ -2,50 +2,75 @@
 package netp;
 
 import java.util.LinkedList;
-import java.util.Queue;
 
-import ca.uqac.lif.cep.Processor;
-import ca.uqac.lif.cep.SingleProcessor;
+import ca.uqac.lif.cep.functions.UnaryFunction;
 
-public class AllDifferent extends SingleProcessor {
+/**
+ * Compares every previous entries on a one-to-one basis. This function takes 
+ * width as an argument which specifies the number of entries to compare.
+ * Complexity O(n)
+ */
+public class AllDifferent extends UnaryFunction<Object, Boolean> {
 
-	int m_width;
+	private int m_width;
+	private int m_falseCount;
+	
+	private boolean allDifferent;
 
-	LinkedList<Object> m_previousInputs;
+	private LinkedList<Object> m_previousInputs;
 
+	/**
+	 * Instantiate this function with an infinite width.
+	 */
 	public AllDifferent() {
-		super(1, 1);
-		m_width = 0;
-		m_previousInputs = new LinkedList<Object>();
+		this(0);
 	}
 
+	/**
+	 * @param width The number of entries to compare one-to-one. 0 or less means infinite width.
+	 */
 	public AllDifferent(int width) {
-		super(1, 1);
-		m_width = width;
+		super(Object.class, Boolean.class);
+		m_width = Math.max(0, width);
+		m_falseCount = 0;
 		m_previousInputs = new LinkedList<Object>();
 	}
 
 	@Override
-	protected Queue<Object[]> compute(Object[] inputs) {
-		Object[] out = new Object[1];
-		out[0] = Boolean.TRUE;
-		for (Object previous : m_previousInputs) {
-			if (previous.equals(inputs[0])) {
-				out[0] = Boolean.FALSE;
+	public Boolean getValue(Object x) {
+		
+		// if a duplicate entry has been found and the width is infinite.
+		if (m_falseCount > 0 && m_width <= 0)
+			return false;
+		
+		allDifferent = true;
+		
+		// if duplicate entries were found in previous calls
+		if (m_falseCount > 0) {
+			m_falseCount--;
+			allDifferent = false;
+		}
+		
+		// compare this entry to every previous one
+		for (int i = m_previousInputs.size() - 1; i >= 0; i--) {
+			if (m_previousInputs.get(i).equals(x)){
+				m_falseCount = Math.max(m_falseCount, i);  // this function will return false at least until this entry is removed
+				allDifferent = false;
 				break;
 			}
 		}
-		if (m_width > 0 && m_previousInputs.size() >= m_width){
+		
+		// update previous entries
+		m_previousInputs.add(x);
+		if (m_width > 0 && m_previousInputs.size() >= m_width) {
 			m_previousInputs.remove();
 		}
-		m_previousInputs.add(inputs[0]);
-		return wrapObject(out);
+		
+		return allDifferent;
 	}
-
-	@Override
-	public Processor clone() {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public int getWidth(){
+		return m_width;
 	}
 
 }
